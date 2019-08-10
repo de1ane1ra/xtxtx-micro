517082406e4c6be61458095efe5f487ff3f9b3da
Josh Avanier <joshavanier@protonmail.com>
2019-08-10 22:17:21 +0700

    Improve colouring

--- down.js
+++ down.js
@@ -1,21 +1,26 @@
+const isHex = value => /^#[0-9a-f]{3}(?:[0-9a-f]{3})?$/i.test(value);
 const editor = document.getElementById('e');
-const {hash} = window.location;
 const lsKey = 'down';
 
-if (hash) {
-  const isHex = value => /^#[0-9a-f]{3}(?:[0-9a-f]{3})?$/i.test(value);
-  let [bg, fg] = hash.split('-');
-  fg = `#${fg}`;
+function paint () {
+  const {hash} = window.location;
+  if (hash) {
+    let [bg, fg] = hash.split('-');
+    fg = `#${fg}`;
 
-  if (isHex(bg) && isHex(fg)) {
-    const css = document.styleSheets[0];
-    css.deleteRule(1);
-    css.insertRule(`::selection{background:${fg};color:${bg}}`, 1);
-    Object.assign(document.body.style, {
-      backgroundColor: bg, color: fg
-    });
+    if (isHex(bg) && isHex(fg)) {
+      const css = document.styleSheets[0];
+      css.deleteRule(1);
+      css.insertRule(`::selection{background:${fg};color:${bg}}`, 1);
+      Object.assign(document.body.style, {
+        backgroundColor: bg, color: fg
+      });
+    }
   }
 }
 
-editor.value = localStorage.getItem(lsKey) || 'Hello.';
+window.onhashchange = paint;
 window.onkeyup = _ => localStorage.setItem(lsKey, editor.value);
+
+paint();
+editor.value = localStorage.getItem(lsKey) || 'Hello.';
