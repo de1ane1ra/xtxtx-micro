37f92015af56b0fc9e8412e84ee010963e63892c
Josh Avanier <joshavanier@protonmail.com>
2019-08-11 18:23:15 +0700

    Clean

--- down.js
+++ down.js
@@ -1,48 +1,52 @@
-const isHex = value => /^#[0-9a-f]{3}(?:[0-9a-f]{3})?$/i.test(value);
 const editor = document.getElementById('e');
 const lsKey = 'down';
 
-function paint () {
-  const {hash} = window.location;
-  if (hash) {
-    let [bg, fg] = hash.split('-');
-    fg = `#${fg}`;
-
-    if (isHex(bg) && isHex(fg)) {
-      const css = document.styleSheets[0];
-      css.deleteRule(1);
-      css.insertRule(`::selection{background:${fg};color:${bg}}`, 1);
-      Object.assign(document.body.style, {
-        backgroundColor: bg, color: fg
-      });
-    }
-  }
+function addCSS (bg, fg) {
+  const css = document.styleSheets[0];
+  css.deleteRule(1);
+  css.insertRule(`::selection{background-color:${fg};color:${bg}}`, 1);
 }
 
-window.onhashchange = paint;
-window.onkeyup = _ => localStorage.setItem(lsKey, editor.value);
+function paint (bg, fg) {
+  Object.assign(document.body.style, {
+    backgroundColor: bg, color: fg
+  });
+}
 
-document.onkeydown = function (e) {
-  if (e.ctrlKey && e.keyCode === 83) {
-    e.preventDefault();
-    e.stopPropagation();
-  }
+function parseHash () {
+  const isHex = value => /^#[0-9a-f]{3}(?:[0-9a-f]{3})?$/i.test(value);
+  const {hash} = window.location;
+  if (!hash) return;
+  let [bg, fg] = hash.split('-');
+  fg = `#${fg}`;
+  if (!isHex(bg) || !isHex(fg)) return;
+  addCSS(bg, fg);
+  paint(bg, fg);
+}
+
+window.onhashchange = parseHash;
+document.onkeyup = () => localStorage.setItem(lsKey, editor.value);
+
+// Prevent Ctrl+S from interrupting
+document.onkeydown = function (event) {
+  if (!(event.ctrlKey && event.keyCode === 83)) return;
+  event.preventDefault();
 };
 
-editor.onkeydown = function (e) {
-  if (e.keyCode === 9) { // Tab
-    e.preventDefault();
-    const start = this.selectionStart;
-    const end = this.selectionEnd;
-    const {target} = e;
-    const {value} = target;
+// Enable tab characters
+editor.onkeydown = function (event) {
+  if (event.keyCode !== 9) return;
+  event.preventDefault();
 
-    target.value = `${value.substring(0, start)}\t${value.substring(end)}`;
+  const start = this.selectionStart;
+  const end = this.selectionEnd;
+  const {target, target: {value}} = event;
 
-    this.selectionStart = start + 1;
-    this.selectionEnd = start + 1;
-  }
+  target.value = `${value.substring(0, start)}\t${value.substring(end)}`;
+
+  this.selectionStart = start + 1;
+  this.selectionEnd = this.selectionStart;
 };
 
-paint();
+parseHash();
 editor.value = localStorage.getItem(lsKey) || 'Hello.';
